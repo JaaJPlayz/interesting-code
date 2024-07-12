687e68431d4fdedb2ea23510ccc2ee0f40921208
JaaJPlayz <hiagoaugustofcunha@gmail.com>
2024-07-11 21:50:18 -0300

    Feat: Crazy stuff ahead, beware of mental failure to the ones who read lol ⚠🤣

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [content, setContent] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/results.txt");
+        const text = await response.text();
+        setContent(text);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>{content}</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/calculations/main.c
+++ src/calculations/main.c
@@ -1,14 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
+#include <string.h>
+#include <libgen.h>
 
 int add(int a, int b) {
   return a + b;
 }
 
+int writeToFile(char* content) {
+  FILE* fptr;
+
+  char* dir;
+
+  char cwd[1024];
+  if (getcwd(cwd, sizeof(cwd)) == NULL) {
+    return 1;
+  } 
+
+  dir = dirname(cwd);
+  dir = dirname(dir);
+
+  char* finalPath = strcat(dir, "/public/results.txt");
+
+  fptr = fopen(finalPath, "w");
+
+  if (fptr == NULL) {
+    return 1;
+  }
+
+  fprintf(fptr, content);
+  fclose(fptr);
+  
+  return 0;
+}
 
 int main(void) {
   srand(time(NULL));
+  char buffer[1024];
   int numbers[2]; 
 
   for (int i = 0; i < 2; i++) {
@@ -16,7 +46,9 @@ int main(void) {
   }
 
   for (int i = 0; i < 10; i++) {
-    printf("%d + %d = %d\n", numbers[0], numbers[1], add(numbers[0], numbers[1]));
+    sprintf(buffer, "%d + %d = %d\n", numbers[0], numbers[1], add(numbers[0], numbers[1]));
+    writeToFile(buffer);
   }
+
   return 0;
 }
